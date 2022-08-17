8b7e2fcff714b9624196acb79ebb17ea750297f7
chechitox11 <89945478+chechitox11@users.noreply.github.com>
2022-08-17 09:28:13 -0500

    Update mult_main.c

--- tarea 1/multiplicacion/mult_main.c
+++ tarea 1/multiplicacion/mult_main.c
@@ -1,4 +1,10 @@
-
+/**************************
+* Fecha: 10/08/2022
+* Autor: Sergio Alejandro Cortes
+* Materia: Parallel and Distributed Computing
+* Tema: Diseño de Benchmark
+* Tarea 1, problema 2
+***************************/
 #include "mult_lib.h"
 #include <math.h>
 #include <stdlib.h>
