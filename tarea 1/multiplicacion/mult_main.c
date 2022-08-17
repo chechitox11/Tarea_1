73d916d79ade6ed37ac36d648bab15c20f782f45
chechitox11 <89945478+chechitox11@users.noreply.github.com>
2022-08-17 09:27:09 -0500

    Update mult_main.c

--- tarea 1/multiplicacion/mult_main.c
+++ tarea 1/multiplicacion/mult_main.c
@@ -1,13 +1,4 @@
 
-/********************************************
-*Fecha: 3 Agosto 2022
-*Autor: Sebastian Ruiz
-*Materia: Prallel and Distributed Computing
-*Tema: Introduccion a la Programacion en C
-*Organizacion por funciones de la App
-*Topico: Aplicacion para el calculo del
-*Indice de Masa Corporal <Body Mass Index>
-********************************************/
 #include "mult_lib.h"
 #include <math.h>
 #include <stdlib.h>
@@ -16,9 +7,8 @@
 
 int main()
 {
-	
-	float no1,no2,multiplicacion;	
-	void bienvenida();
+	bienvenida();
+	multiplicar();
 	return 0;
 }
 
