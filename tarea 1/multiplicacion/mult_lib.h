673c27c143f772e96ed93a74b00993f920ea1260
chechitox11 <89945478+chechitox11@users.noreply.github.com>
2022-08-17 09:31:21 -0500

    Update mult_lib.h

--- tarea 1/multiplicacion/mult_lib.h
+++ tarea 1/multiplicacion/mult_lib.h
@@ -1,13 +1,10 @@
-
-/********************************************
-*Fecha: 3 Agosto 2022
-*Autor: Sebastian Ruiz
-*Materia: Prallel and Distributed Computing
-*Tema: Introduccion a la Programacion en C
-*Organizacion por funciones de la App
-*Topico: Aplicacion para el calculo del
-*Indice de Masa Corporal <Body Mass Index>
-********************************************/
+/**************************
+* Fecha: 16/08/2022
+* Autor: Sergio Alejandro Cortes
+* Materia: Parallel and Distributed Computing
+* Tema: Diseño de Benchmark
+* Tarea 1, problema 1
+***************************/
 #ifndef BMI_LIB_H_INCLUDED
 #define BMI_LIB_H_INCLUDED
 #include <math.h>
